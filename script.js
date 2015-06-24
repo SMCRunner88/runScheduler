dd6237c20f6802a93ed30ec0fd149c0dafa9b4fe
smcrunner88 <smcrunner88@gmail.com>
2015-06-24 07:41:06 -0700

    Added js, html, and CSS files

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+var weeklyMileage = prompt("How many miles a week do you want to run?");
+var longRunPercent = prompt("What percent of your week mileage is your long run? (Recommended 20-20%)");
+var numberOfSessions = prompt("How many times a week can you run? (1-13)");*/
+
+var longRun = weeklyMileage * longRunPercent;
+var normalRun = (weeklyMileage-longRun) / numberOfSessions;
+
+
+
+
