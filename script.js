5ac661a1976c8feda8de5d510a3befe0c9f3bfce
Scott Cook <smcrunner88@gmail.com>
2015-08-14 09:51:36 -0700

    Changes made to html and css files. Added a form for a user to fill out rather than using 'alerts' from JS

--- script.js
+++ script.js
@@ -3,13 +3,12 @@
 
 
 
-var weeklyMileage = parseInt(prompt('How many miles do you want to run this week?'));
-var longRunPercent = parseInt(prompt('What percent of your weekly mileage do you like your long runs? (Recommended 20-25)'));
-var sessionsNum = parseInt(prompt('How many times a week can you run (0-13)?'));
+var weeklyMileage = document.getElementbyId('mileage').value
+var longRunPercent = //parseInt(prompt('What percent of your weekly mileage do you like your long runs? (Recommended 20-25)'));
+var sessionsNum = //parseInt(prompt('How many times a week can you run (0-13)?'));
 
 function morningRun () {
-  if (sessionsNum > 7) {
-    var morningRunSessions = sessionsNum - 7;
+  if (sessionsNum > 7) {    var morningRunSessions = sessionsNum - 7;
 
   }
   return morningRunSessions;
@@ -21,8 +20,8 @@ var morningRunMiles = morningRunSessions * 4;
 var normalRun = Math.ceil((weeklyMileage-longRun-morningRunMiles) / (sessionsNum-morningRunSessions-1));
 
 
-document.write("These are the recommended distances your runs should be<br>");
+/*document.write("These are the recommended distances your runs should be<br>");
 document.write("Long Run: " + longRun + "<br>");
 document.write("Regular Runs: " + normalRun + "<br>");
 document.write("You should have " + morningRunSessions + " morning runs that are 4 miles each")
-console.log(morningRunSessions);
+console.log(morningRunSessions);*/
